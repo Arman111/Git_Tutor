a64b3d12f2950fdc1056fabc520cec7210afe09c
Arman111 <hayrapetyan.arman@hotmail.com>
2017-03-19 13:11:42 +0400

    Project setup

---
+++ Git Tutor/Git Tutor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Tutor
+//
+//  Created by Arman on 3/19/17.
+//  Copyright © 2017 Arman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
